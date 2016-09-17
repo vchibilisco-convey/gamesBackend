4e1daae4ec2673a85c52acf1359c25f9cdda421c
Gonzalo Mercado <gonmercado@gmail.com>
2016-09-17 19:29:56 -0300

    Adding logger for requests.

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var morgan = require('morgan');
+var fs = require('fs');
+var path = require('path');
 require('../classes');
 
+var accessLogStream = fs.createWriteStream(path.join(__dirname, '../logs/access.log'), {flags: 'a'});
+
+router.use(morgan('combined', {stream: accessLogStream}));
 router.use(require('../middlewares/auth'));
-router.use(require('../middlewares/log'));
 router.use('/games', require('./game.controller'));
 router.use(require('../middlewares/error'));
 
