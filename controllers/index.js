165818b5a41fa5c116c01b63c4d386226106485a
Gonzalo Mercado <gonmercado@gmail.com>
2016-09-17 19:55:45 -0300

    Cleaning up the routes files

--- controllers/index.js
+++ controllers/index.js
@@ -1,19 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var morgan = require('morgan');
-var fs = require('fs');
-var path = require('path');
 require('../classes');
 
-var accessLogStream = fs.createWriteStream(path.join(__dirname, '../logs/access.log'), {flags: 'a'});
-
-router.use(morgan('combined', {stream: accessLogStream}));
+//Middlewares before requests
+router.use(require('../middlewares/accessLog'));
 router.use(require('../middlewares/auth'));
-router.use('/games', require('./game.controller'));
-router.use(require('../middlewares/error'));
 
-router.get('/', function(req, res){
-  res.json({ message: 'Welcome to the games API'});
-});
+//Application routes.
+router.get('/', require('./root.controller'));
+router.use('/games', require('./game.controller'));
+
+//Middlewares after requests
+router.use(require('../middlewares/error'));
 
 module.exports = router;
